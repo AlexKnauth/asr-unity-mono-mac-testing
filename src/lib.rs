e0e087236cdc1aab16b91d582964bb60bf6a6bef
AlexKnauth <alexander@knauth.org>
2023-09-12 17:51:58 -0400

    Add detect_version

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ use std::{path::Path, fs::File, io::{Read, self}, mem, slice};
 
 use asr::{future::next_tick, Process, Address, string::ArrayCString, signature::Signature, Address64, game_engine::unity::mono};
 use bytemuck::CheckedBitPattern;
+use memchr::memmem;
 
 asr::async_main!(stable);
 
@@ -10,6 +11,17 @@ asr::async_main!(stable);
 const MONO_GET_ROOT_DOMAIN: &str = "_mono_get_root_domain";
 const MONO_GET_ROOT_DOMAIN_LEN: usize = MONO_GET_ROOT_DOMAIN.len();
 const MONO_GET_ROOT_DOMAIN_LEN_1: usize = MONO_GET_ROOT_DOMAIN_LEN + 1;
+
+const UNITY_PLAYER_VERSION: &[u8] = b"Unity Player version ";
+const UNITY_PLAYER_VERSION_LEN: usize = UNITY_PLAYER_VERSION.len();
+
+/* Constant for the magic field of the mach_header_32 (32-bit architectures) */
+const MH_MAGIC_32: u32 = 0xfeedface; /* the mach magic number */
+const MH_CIGAM_32: u32 = 0xcefaedfe; /* NXSwapInt(MH_MAGIC) */
+
+/* Constant for the magic field of the mach_header_64 (64-bit architectures) */
+const MH_MAGIC_64: u32 = 0xfeedfacf; /* the 64-bit mach magic number */
+const MH_CIGAM_64: u32 = 0xcffaedfe; /* NXSwapInt(MH_MAGIC_64) */
 
 // --------------------------------------------------------
 
@@ -223,7 +235,7 @@ async fn main() {
         let process = Process::wait_attach("Hollow Knight").await;
         process
             .until_closes(async {
-                let a = attach(&process);
+                let a = attach_auto_detect_default(&process);
 
                 // TODO: Load some initial information from the process.
                 asr::print_message(&format!("done: {:?}", a));
@@ -236,108 +248,90 @@ async fn main() {
     }
 }
 
-fn attach(process: &Process) -> Option<Address> {
-    // TODO: Attach Unity / Mono stuff with code similar to
-    // GetRootDomainFunctionAddressMachOFormat from:
-    // https://github.com/hackf5/unityspy/blob/master/src/HackF5.UnitySpy/AssemblyImageFactory.cs#L160
+fn attach_auto_detect_default(process: &Process) -> Option<(Address, Address)> {
+    let version = detect_version(process)?;
+    asr::print_message(&format!("version detected: {:?}", version));
+    attach_default(process, version)
+}
+
+fn attach_default(process: &Process, version: mono::Version) -> Option<(Address, Address)> {
+    attach(process, version, "Assembly-CSharp")
+}
+
+fn attach(process: &Process, version: mono::Version, assembly_name: &str) -> Option<(Address, Address)> {
     let mono_module = process.get_module_range("libmonobdwgc-2.0.dylib").ok()?;
     let (mono_module_addr, mono_module_len) = mono_module;
     let process_path = process.get_path().ok()?;
     let contents_path = Path::new(&process_path).parent()?.parent()?;
     let mono_module_path = contents_path.join("Frameworks").join("libmonobdwgc-2.0.dylib");
     let module_from_path = file_read_all_bytes(mono_module_path).ok()?;
-    let macho_offsets = MachOFormatOffsets::new();
-    let number_of_commands: u32 = slice_read(&module_from_path, macho_offsets.number_of_commands)?;
-
-    let mut root_domain_function_address = Address::NULL;
-
-    let mut offset_to_next_command: usize = macho_offsets.load_commands as usize;
-    for _i in 0..number_of_commands {
-        // Check if load command is LC_SYMTAB
-        let next_command: i32 = slice_read(&module_from_path, offset_to_next_command)?;
-        if next_command == 2 {
-            let symbol_table_offset: u32 = slice_read(&module_from_path, offset_to_next_command + macho_offsets.symbol_table_offset)?;
-            let number_of_symbols: u32 = slice_read(&module_from_path, offset_to_next_command + macho_offsets.number_of_symbols)?;
-            let string_table_offset: u32 = slice_read(&module_from_path, offset_to_next_command + macho_offsets.string_table_offset)?;
-
-            for j in 0..(number_of_symbols as usize) {
-                let symbol_name_offset: u32 = slice_read(&module_from_path, symbol_table_offset as usize + (j * macho_offsets.size_of_nlist_item))?;
-                let symbol_name: ArrayCString<MONO_GET_ROOT_DOMAIN_LEN_1> = slice_read(&module_from_path, (string_table_offset + symbol_name_offset) as usize)?;
-
-                if symbol_name.matches(MONO_GET_ROOT_DOMAIN) {
-                    let root_domain_function_offset: u32 = slice_read(&module_from_path, symbol_table_offset as usize + (j * macho_offsets.size_of_nlist_item) + macho_offsets.nlist_value)?;
-                    asr::print_message(&format!("root_domain_function_offset: {}", root_domain_function_offset));
-                    asr::print_message(&format!("mono_module_len: {}", mono_module_len));
-                    root_domain_function_address = mono_module_addr + root_domain_function_offset;
-                    break;
-                }
-            }
-
-            break;
-        } else {
-            let command_size: u32 = slice_read(&module_from_path, offset_to_next_command + macho_offsets.command_size)?;
-            offset_to_next_command += command_size as usize;
-        }
-    }
-
-    if root_domain_function_address.is_null() {
-        return None;
-    }
-
-    if process.read::<u8>(root_domain_function_address).is_ok() {
-        return Some(root_domain_function_address);
-    }
-
-    let root_domain_function_offset_in_page = root_domain_function_address.value() & 0xfff;
-    let number_of_pages = mono_module_len / 0x1000;
-    const SIG_MONO_64: Signature<3> = Signature::new("48 8B 0D");
-    asr::print_message("looking at offset in page...");
-    for i in 0..number_of_pages {
-        let a = Address::new(mono_module_addr.value() + (i * 0x1000) + root_domain_function_offset_in_page);
-        if process.read::<u8>(a).is_ok() {
-            let mb = SIG_MONO_64.scan_process_range(process, (a, 0x100));
-            if let Some(b) = mb {
-                let scan_address = b + 3;
-                let mc = process.read::<i32>(scan_address).ok();
-                if let Some(c) = mc {
-                    let assemblies = scan_address + 0x4 + c;
-                    if attach_assemblies(process, assemblies).is_some() {
-                        asr::print_message("found at offset in page.");
-                        return Some(a);
-                    }
-                }
-            }
-        }
-    }
-
-    asr::print_message("looking everywhere else...");
+    let magic: u32 = slice_read(&module_from_path, 0)?;
+    let is_64_bit = match magic {
+        MH_MAGIC_64 | MH_CIGAM_64 => true,
+        MH_MAGIC_32 | MH_CIGAM_32 => false,
+        _ => { return None; }
+    };
 
     for i in 0..(mono_module_len/8) {
-        let a = Address::new(mono_module_addr.value() + (i * 8));
-        if attach_assemblies(process, a).is_some() {
-            asr::print_message("found somewhere else.");
-            return Some(a);
+        let a = mono_module_addr + (i * 8);
+        if let Some(image) = attach_assemblies(process, a, is_64_bit, version, assembly_name) {
+            return Some((a, image));
         }
     }
 
     None
 }
 
-fn attach_assemblies(process: &Process, assemblies_addr: Address) -> Option<Address> {
-    let offsets = Offsets::new(mono::Version::V2, true);
+fn attach_assemblies(process: &Process, assemblies_addr: Address, is_64_bit: bool, version: mono::Version, assembly_name: &str) -> Option<Address> {
+    let offsets = Offsets::new(version, is_64_bit);
     let mut assemblies = process.read::<Address64>(assemblies_addr).ok()?;
     let image = loop {
         let data = process.read::<Address64>(assemblies).ok()?;
         if data.is_null() { return None; }
         let name_addr = process.read::<Address64>(data + offsets.monoassembly_aname as u64 + offsets.monoassemblyname_name as u64).ok()?;
         let name = process.read::<ArrayCString<128>>(name_addr).ok()?;
-        if name.matches("Assembly-CSharp") {
-            asr::print_message("name.matches(\"Assembly-CSharp\")");
+        if name.matches(assembly_name) {
             break process.read::<Address64>(data + offsets.monoassembly_image as u64).ok()?;
         }
         assemblies = process.read::<Address64>(assemblies + offsets.glist_next as u64).ok()?;
     };
     Some(image.into())
+}
+
+fn detect_version(process: &Process) -> Option<mono::Version> {
+    let process_path = process.get_path().ok()?;
+    let contents_path = Path::new(&process_path).parent()?.parent()?;
+    let info_plist_path = contents_path.join("Info.plist");
+    let info_plist_bytes = file_read_all_bytes(info_plist_path).ok()?;
+    // example: "Unity Player version 2020.2.2f1 "
+    let upv = memmem::find(&info_plist_bytes, UNITY_PLAYER_VERSION)?;
+    let version_string: ArrayCString<6> = slice_read(&info_plist_bytes, upv + UNITY_PLAYER_VERSION_LEN)?;
+    let (before, after) = version_string.split_at(version_string.iter().position(|&x| x == b'.')?);
+
+    const ZERO: u8 = b'0';
+    const NINE: u8 = b'9';
+
+    let mut unity: u32 = 0;
+    for &val in before {
+        match val {
+            ZERO..=NINE => unity = unity * 10 + (val - ZERO) as u32,
+            _ => break,
+        }
+    }
+
+    let mut unity_minor: u32 = 0;
+    for &val in &after[1..] {
+        match val {
+            ZERO..=NINE => unity_minor = unity_minor * 10 + (val - ZERO) as u32,
+            _ => break,
+        }
+    }
+
+    Some(if (unity == 2021 && unity_minor >= 2) || (unity > 2021) {
+        mono::Version::V3
+    } else {
+        mono::Version::V2
+    })
 }
 
 fn file_read_all_bytes<P: AsRef<Path>>(path: P) -> io::Result<Vec<u8>> {
