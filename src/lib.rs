0f49aca8e65ef238c26967550344c18090386ae6
AlexKnauth <alexander@knauth.org>
2023-09-12 16:47:25 -0400

    Add attach_assemblies

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use std::{path::Path, fs::File, io::{Read, self}, mem, slice};
 
-use asr::{future::next_tick, Process, Address, string::ArrayCString};
+use asr::{future::next_tick, Process, Address, string::ArrayCString, signature::Signature, Address64, game_engine::unity::mono};
 use bytemuck::CheckedBitPattern;
 
 asr::async_main!(stable);
@@ -38,6 +38,172 @@ impl MachOFormatOffsets {
             string_table_offset: 0x10,
             nlist_value: 0x08,
             size_of_nlist_item: 0x10,
+        }
+    }
+}
+
+struct Offsets {
+    monoassembly_aname: u8,
+    monoassembly_image: u8,
+    monoassemblyname_name: u8,
+    glist_next: u8,
+    monoimage_class_cache: u16,
+    monointernalhashtable_table: u8,
+    monointernalhashtable_size: u8,
+    monoclassdef_next_class_cache: u16,
+    monoclassdef_klass: u8,
+    monoclass_name: u8,
+    monoclass_fields: u8,
+    monoclassdef_field_count: u16,
+    monoclass_runtime_info: u8,
+    monoclass_vtable_size: u8,
+    monoclass_parent: u8,
+    monoclassfield_name: u8,
+    monoclassfield_offset: u8,
+    monoclassruntimeinfo_domain_vtables: u8,
+    monovtable_vtable: u8,
+    monoclassfieldalignment: u8,
+}
+
+impl Offsets {
+    const fn new(version: mono::Version, is_64_bit: bool) -> &'static Self {
+        match is_64_bit {
+            true => match version {
+                mono::Version::V1 => &Self {
+                    monoassembly_aname: 0x10,
+                    monoassembly_image: 0x58,
+                    monoassemblyname_name: 0x0,
+                    glist_next: 0x8,
+                    monoimage_class_cache: 0x3D0,
+                    monointernalhashtable_table: 0x20,
+                    monointernalhashtable_size: 0x18,
+                    monoclassdef_next_class_cache: 0x100,
+                    monoclassdef_klass: 0x0,
+                    monoclass_name: 0x48,
+                    monoclass_fields: 0xA8,
+                    monoclassdef_field_count: 0x94,
+                    monoclass_runtime_info: 0xF8,
+                    monoclass_vtable_size: 0x18, // MonoVtable.data
+                    monoclass_parent: 0x30,
+                    monoclassfield_name: 0x8,
+                    monoclassfield_offset: 0x18,
+                    monoclassruntimeinfo_domain_vtables: 0x8,
+                    monovtable_vtable: 0x48,
+                    monoclassfieldalignment: 0x20,
+                },
+                mono::Version::V2 => &Self {
+                    monoassembly_aname: 0x10,
+                    monoassembly_image: 0x60,
+                    monoassemblyname_name: 0x0,
+                    glist_next: 0x8,
+                    monoimage_class_cache: 0x4C0,
+                    monointernalhashtable_table: 0x20,
+                    monointernalhashtable_size: 0x18,
+                    monoclassdef_next_class_cache: 0x108,
+                    monoclassdef_klass: 0x0,
+                    monoclass_name: 0x48,
+                    monoclass_fields: 0x98,
+                    monoclassdef_field_count: 0x100,
+                    monoclass_runtime_info: 0xD0,
+                    monoclass_vtable_size: 0x5C,
+                    monoclass_parent: 0x30,
+                    monoclassfield_name: 0x8,
+                    monoclassfield_offset: 0x18,
+                    monoclassruntimeinfo_domain_vtables: 0x8,
+                    monovtable_vtable: 0x40,
+                    monoclassfieldalignment: 0x20,
+                },
+                mono::Version::V3 => &Self {
+                    monoassembly_aname: 0x10,
+                    monoassembly_image: 0x60,
+                    monoassemblyname_name: 0x0,
+                    glist_next: 0x8,
+                    monoimage_class_cache: 0x4D0,
+                    monointernalhashtable_table: 0x20,
+                    monointernalhashtable_size: 0x18,
+                    monoclassdef_next_class_cache: 0x108,
+                    monoclassdef_klass: 0x0,
+                    monoclass_name: 0x48,
+                    monoclass_fields: 0x98,
+                    monoclassdef_field_count: 0x100,
+                    monoclass_runtime_info: 0xD0,
+                    monoclass_vtable_size: 0x5C,
+                    monoclass_parent: 0x30,
+                    monoclassfield_name: 0x8,
+                    monoclassfield_offset: 0x18,
+                    monoclassruntimeinfo_domain_vtables: 0x8,
+                    monovtable_vtable: 0x48,
+                    monoclassfieldalignment: 0x20,
+                },
+            },
+            false => match version {
+                mono::Version::V1 => &Self {
+                    monoassembly_aname: 0x8,
+                    monoassembly_image: 0x40,
+                    monoassemblyname_name: 0x0,
+                    glist_next: 0x4,
+                    monoimage_class_cache: 0x2A0,
+                    monointernalhashtable_table: 0x14,
+                    monointernalhashtable_size: 0xC,
+                    monoclassdef_next_class_cache: 0xA8,
+                    monoclassdef_klass: 0x0,
+                    monoclass_name: 0x30,
+                    monoclass_fields: 0x74,
+                    monoclassdef_field_count: 0x64,
+                    monoclass_runtime_info: 0xA4,
+                    monoclass_vtable_size: 0xC, // MonoVtable.data
+                    monoclass_parent: 0x24,
+                    monoclassfield_name: 0x4,
+                    monoclassfield_offset: 0xC,
+                    monoclassruntimeinfo_domain_vtables: 0x4,
+                    monovtable_vtable: 0x28,
+                    monoclassfieldalignment: 0x10,
+                },
+                mono::Version::V2 => &Self {
+                    monoassembly_aname: 0x8,
+                    monoassembly_image: 0x44,
+                    monoassemblyname_name: 0x0,
+                    glist_next: 0x4,
+                    monoimage_class_cache: 0x354,
+                    monointernalhashtable_table: 0x14,
+                    monointernalhashtable_size: 0xC,
+                    monoclassdef_next_class_cache: 0xA8,
+                    monoclassdef_klass: 0x0,
+                    monoclass_name: 0x2C,
+                    monoclass_fields: 0x60,
+                    monoclassdef_field_count: 0xA4,
+                    monoclass_runtime_info: 0x84,
+                    monoclass_vtable_size: 0x38,
+                    monoclass_parent: 0x20,
+                    monoclassfield_name: 0x4,
+                    monoclassfield_offset: 0xC,
+                    monoclassruntimeinfo_domain_vtables: 0x4,
+                    monovtable_vtable: 0x28,
+                    monoclassfieldalignment: 0x10,
+                },
+                mono::Version::V3 => &Self {
+                    monoassembly_aname: 0x8,
+                    monoassembly_image: 0x48,
+                    monoassemblyname_name: 0x0,
+                    glist_next: 0x4,
+                    monoimage_class_cache: 0x35C,
+                    monointernalhashtable_table: 0x14,
+                    monointernalhashtable_size: 0xC,
+                    monoclassdef_next_class_cache: 0xA0,
+                    monoclassdef_klass: 0x0,
+                    monoclass_name: 0x2C,
+                    monoclass_fields: 0x60,
+                    monoclassdef_field_count: 0x9C,
+                    monoclass_runtime_info: 0x7C,
+                    monoclass_vtable_size: 0x38,
+                    monoclass_parent: 0x20,
+                    monoclassfield_name: 0x4,
+                    monoclassfield_offset: 0xC,
+                    monoclassruntimeinfo_domain_vtables: 0x4,
+                    monovtable_vtable: 0x2C,
+                    monoclassfieldalignment: 0x10,
+                },
+            },
         }
     }
 }
@@ -118,21 +284,60 @@ fn attach(process: &Process) -> Option<Address> {
         return None;
     }
 
-    asr::print_message(&format!("mono_module_addr: {}", mono_module_addr));
-    asr::print_message(&format!("root_domain_function_address: {}", root_domain_function_address));
-    let b = Address::new(mono_module_addr.value() + (root_domain_function_address.value() - mono_module_addr.value())*0xd087dc/0x1000000);
-    let c = Address::new(mono_module_addr.value() + (root_domain_function_address.value() - mono_module_addr.value())*0xd087dd/0x1000000);
-    let d = Address::new(mono_module_addr.value() + (root_domain_function_address.value() - mono_module_addr.value())*0xd157a0/0x1000000);
-    let f = Address::new(mono_module_addr.value() + mono_module_len);
-    let av = process.read::<u8>(mono_module_addr);
-    let bv = process.read::<u8>(b);
-    let cv = process.read::<u8>(c);
-    let dv = process.read::<u8>(d);
-    let ev = process.read::<u8>(root_domain_function_address);
-    asr::print_message(&format!("av: {:?}, bv: {:?}, cv: {:?}, dv: {:?}, ev: {:?}", av, bv, cv, dv, ev));
-    asr::print_message(&format!("a: {:?}\nb: {:?}\nc: {:?}\nd: {:?}\ne: {:?}\nf: {:?}", mono_module_addr, b, c, d, root_domain_function_address, f));
+    if process.read::<u8>(root_domain_function_address).is_ok() {
+        return Some(root_domain_function_address);
+    }
 
-    Some(root_domain_function_address)
+    let root_domain_function_offset_in_page = root_domain_function_address.value() & 0xfff;
+    let number_of_pages = mono_module_len / 0x1000;
+    const SIG_MONO_64: Signature<3> = Signature::new("48 8B 0D");
+    asr::print_message("looking at offset in page...");
+    for i in 0..number_of_pages {
+        let a = Address::new(mono_module_addr.value() + (i * 0x1000) + root_domain_function_offset_in_page);
+        if process.read::<u8>(a).is_ok() {
+            let mb = SIG_MONO_64.scan_process_range(process, (a, 0x100));
+            if let Some(b) = mb {
+                let scan_address = b + 3;
+                let mc = process.read::<i32>(scan_address).ok();
+                if let Some(c) = mc {
+                    let assemblies = scan_address + 0x4 + c;
+                    if attach_assemblies(process, assemblies).is_some() {
+                        asr::print_message("found at offset in page.");
+                        return Some(a);
+                    }
+                }
+            }
+        }
+    }
+
+    asr::print_message("looking everywhere else...");
+
+    for i in 0..(mono_module_len/8) {
+        let a = Address::new(mono_module_addr.value() + (i * 8));
+        if attach_assemblies(process, a).is_some() {
+            asr::print_message("found somewhere else.");
+            return Some(a);
+        }
+    }
+
+    None
+}
+
+fn attach_assemblies(process: &Process, assemblies_addr: Address) -> Option<Address> {
+    let offsets = Offsets::new(mono::Version::V2, true);
+    let mut assemblies = process.read::<Address64>(assemblies_addr).ok()?;
+    let image = loop {
+        let data = process.read::<Address64>(assemblies).ok()?;
+        if data.is_null() { return None; }
+        let name_addr = process.read::<Address64>(data + offsets.monoassembly_aname as u64 + offsets.monoassemblyname_name as u64).ok()?;
+        let name = process.read::<ArrayCString<128>>(name_addr).ok()?;
+        if name.matches("Assembly-CSharp") {
+            asr::print_message("name.matches(\"Assembly-CSharp\")");
+            break process.read::<Address64>(data + offsets.monoassembly_image as u64).ok()?;
+        }
+        assemblies = process.read::<Address64>(assemblies + offsets.glist_next as u64).ok()?;
+    };
+    Some(image.into())
 }
 
 fn file_read_all_bytes<P: AsRef<Path>>(path: P) -> io::Result<Vec<u8>> {
