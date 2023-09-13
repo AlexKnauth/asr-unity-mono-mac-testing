e045e85f2f1321a75a8f48f5f07333c867cb9b73
AlexKnauth <alexander@knauth.org>
2023-09-13 15:21:57 -0400

    Get GameManager sceneName, nextSceneName

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 
-use asr::{future::next_tick, Process, game_engine::unity::mono, Address};
+use std::{cmp::min, collections::BTreeMap};
+
+use asr::{future::next_tick, Process, game_engine::unity::mono, Address, Address64, string::ArrayWString};
 
 asr::async_main!(stable);
 
@@ -19,12 +21,26 @@ async fn main() {
         let process = Process::wait_attach("Hollow Knight").await;
         process
             .until_closes(async {
-                let a = wait_attach_game_manager(&process).await;
+                let (module, _image, game_manager_class, game_manager_instance) = wait_attach_game_manager(&process).await.expect("GameManager");
+                let scene_name_offset = game_manager_class.get_field(&process, &module, "sceneName").expect("sceneName") as u64;
+                let next_scene_name_offset = game_manager_class.get_field(&process, &module, "nextSceneName").expect("nextSceneName") as u64;
 
                 // TODO: Load some initial information from the process.
-                asr::print_message(&format!("done: {:?}", a.is_some()));
+                let mut string_table: BTreeMap<u64, Option<String>> = BTreeMap::new();
+                string_table.insert(scene_name_offset, None);
+                string_table.insert(next_scene_name_offset, None);
                 loop {
                     // TODO: Do something on every tick.
+                    let scene_name = read_pointer_path_string_object::<64>(&process, game_manager_instance, &[scene_name_offset]);
+                    let next_scene_name = read_pointer_path_string_object::<64>(&process, game_manager_instance, &[next_scene_name_offset]);
+                    if Some(&scene_name) != string_table.get(&scene_name_offset) {
+                        string_table.insert(scene_name_offset, scene_name);
+                        asr::print_message(&format!("sceneName: {:?}", string_table.get(&scene_name_offset)));
+                    }
+                    if Some(&next_scene_name) != string_table.get(&next_scene_name_offset) {
+                        string_table.insert(next_scene_name_offset, next_scene_name);
+                        asr::print_message(&format!("nextSceneName: {:?}", string_table.get(&next_scene_name_offset)));
+                    }
                     next_tick().await;
                 }
             })
@@ -50,4 +66,20 @@ fn attach_auto_detect_default(process: &Process) -> Option<(mono::Module, mono::
     let module = mono::Module::attach_auto_detect(process)?;
     let image = module.get_default_image(process)?;
     Some((module, image))
+}
+
+fn read_pointer_path_string_object<const N: usize>(process: &Process, address: Address, path: &[u64]) -> Option<String> {
+    let string_object: Address64 = process.read_pointer_path64(address, path).ok()?;
+    read_string_object::<N>(process, string_object)
+}
+
+fn read_string_object<const N: usize>(process: &Process, a: Address64) -> Option<String> {
+    const STRING_LEN_OFFSET: u64 = 0x10;
+    const STRING_CONTENTS_OFFSET: u64 = 0x14;
+
+    let n: u32 = process.read_pointer_path64(a, &[STRING_LEN_OFFSET]).ok()?;
+    if !(n < 2048) { return None; }
+    let w: ArrayWString<N> = process.read_pointer_path64(a, &[STRING_CONTENTS_OFFSET]).ok()?;
+    if !(w.len() == min(n as usize, N)) { return None; }
+    String::from_utf16(&w.to_vec()).ok()
 }
