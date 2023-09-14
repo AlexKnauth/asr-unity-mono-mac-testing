73a1fcefec73a66fbdf76694ee99f8a727b2fafd
AlexKnauth <alexander@knauth.org>
2023-09-14 18:19:35 -0400

    print SceneManager and GameManager scene names

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 
 use std::{cmp::min, collections::BTreeMap};
 
-use asr::{future::next_tick, Process, game_engine::unity::mono, Address, Address64, string::ArrayWString};
+use asr::{future::next_tick, Process, game_engine::unity::{mono, SceneManager, get_scene_name}, Address, Address64, string::{ArrayWString, ArrayCString}};
 
 asr::async_main!(stable);
 
@@ -21,25 +21,35 @@ async fn main() {
         let process = Process::wait_attach("Hollow Knight").await;
         process
             .until_closes(async {
+                asr::print_message("attaching SceneManager...");
+                for _ in 0..0x10 { next_tick().await; }
+                let scene_manager = SceneManager::wait_attach(&process).await;
+                asr::print_message("attached SceneManager");
                 let (module, _image, game_manager_class, game_manager_instance) = wait_attach_game_manager(&process).await.expect("GameManager");
                 let scene_name_offset = game_manager_class.get_field(&process, &module, "sceneName").expect("sceneName") as u64;
                 let next_scene_name_offset = game_manager_class.get_field(&process, &module, "nextSceneName").expect("nextSceneName") as u64;
 
                 // TODO: Load some initial information from the process.
-                let mut string_table: BTreeMap<u64, Option<String>> = BTreeMap::new();
-                string_table.insert(scene_name_offset, None);
-                string_table.insert(next_scene_name_offset, None);
+                let mut string_table: BTreeMap<(&str, u64), Option<String>> = BTreeMap::new();
+                string_table.insert(("SceneManager", 0), None);
+                string_table.insert(("GameManager", scene_name_offset), None);
+                string_table.insert(("GameManager", next_scene_name_offset), None);
                 loop {
                     // TODO: Do something on every tick.
+                    let scene_manager_scene_name: Option<String> = scene_manager.get_current_scene_path::<64>(&process).ok().map(scene_path_to_name_string);
                     let scene_name = read_pointer_path_string_object::<64>(&process, game_manager_instance, &[scene_name_offset]);
                     let next_scene_name = read_pointer_path_string_object::<64>(&process, game_manager_instance, &[next_scene_name_offset]);
-                    if Some(&scene_name) != string_table.get(&scene_name_offset) {
-                        string_table.insert(scene_name_offset, scene_name);
-                        asr::print_message(&format!("sceneName: {:?}", string_table.get(&scene_name_offset)));
+                    if Some(&scene_manager_scene_name) != string_table.get(&("SceneManager", 0)) {
+                        string_table.insert(("SceneManager", 0), scene_manager_scene_name);
+                        asr::print_message(&format!("SceneManager sceneName: {:?}", string_table.get(&("SceneManager", 0))));
                     }
-                    if Some(&next_scene_name) != string_table.get(&next_scene_name_offset) {
-                        string_table.insert(next_scene_name_offset, next_scene_name);
-                        asr::print_message(&format!("nextSceneName: {:?}", string_table.get(&next_scene_name_offset)));
+                    if Some(&scene_name) != string_table.get(&("GameManager", scene_name_offset)) {
+                        string_table.insert(("GameManager", scene_name_offset), scene_name);
+                        asr::print_message(&format!("GameManager sceneName: {:?}", string_table.get(&("GameManager", scene_name_offset))));
+                    }
+                    if Some(&next_scene_name) != string_table.get(&("GameManager", next_scene_name_offset)) {
+                        string_table.insert(("GameManager", next_scene_name_offset), next_scene_name);
+                        asr::print_message(&format!("GameManager nextSceneName: {:?}", string_table.get(&("GameManager", next_scene_name_offset))));
                     }
                     next_tick().await;
                 }
@@ -84,4 +94,8 @@ fn read_string_object<const N: usize>(process: &Process, a: Address64) -> Option
     let w: ArrayWString<N> = process.read_pointer_path64(a, &[STRING_CONTENTS_OFFSET]).ok()?;
     if !(w.len() == min(n as usize, N)) { return None; }
     String::from_utf16(&w.to_vec()).ok()
+}
+
+pub fn scene_path_to_name_string<const N: usize>(scene_path: ArrayCString<N>) -> String {
+    String::from_utf8(get_scene_name(&scene_path).to_vec()).unwrap()
 }
