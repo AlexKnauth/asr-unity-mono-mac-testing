3475ad4f9c0362c1dceff262aee56636c191c0fe
Alex Knauth <alexander@knauth.org>
2024-01-18 00:16:09 -0500

    Test string-value bool-widget conflict

--- src/lib.rs
+++ src/lib.rs
@@ -17,6 +17,17 @@ async fn main() {
 
     asr::print_message("Hello, World!");
 
+    let m1 = asr::settings::Map::load();
+    m1.insert("b", "this string is not a boolean");
+    m1.store();
+
+    let b2 = asr::settings::gui::add_bool("b", "b should be a bool", true);
+    asr::print_message(&format!("b2: {}", b2));
+
+    let m3 = asr::settings::Map::load();
+    let b3 = m3.get("b");
+    asr::print_message(&format!("b3: {:?}", b3));
+
     loop {
         next_tick().await;
     }
