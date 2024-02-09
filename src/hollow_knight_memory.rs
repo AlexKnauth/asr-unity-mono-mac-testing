a9e3c1fee1fc541b4576721e0e4e5916e3dac65b
AlexKnauth <alexander@knauth.org>
2024-02-09 12:42:45 -0500

    uncomment gameState

--- src/hollow_knight_memory.rs
+++ src/hollow_knight_memory.rs
@@ -57,8 +57,8 @@ static HOLLOW_KNIGHT_POINTERS: &[(&str, (&str, u8, &[&str]), Type)] = &[
 
     ("GameManager sceneName", ("GameManager", 0, &["_instance", "sceneName"]), Type::String),
     ("GameManager nextSceneName", ("GameManager", 0, &["_instance", "nextSceneName"]), Type::String),
-    /*
     ("GameManager gameState", ("GameManager", 0, &["_instance", "gameState"]), Type::I32),
+    /*
     ("GameManager uiState vanilla", ("GameManager", 0, &["_instance", "<ui>k__BackingField", "uiState"]), Type::I32),
     ("GameManager uiState modded", ("GameManager", 0, &["_instance", "_uiInstance", "uiState"]), Type::I32),
     // ("GameManager camera teleporting", ("GameManager", 0, &["_instance", "<cameraCtrl>k__BackingField", "teleporting"]), Type::Bool),
